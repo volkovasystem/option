05fcd4f8dbbcb8bcdb792baeebfae1fe566408ac
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-09-16 22:11:06 +0800

    Added unit test.
    Added test set get option on undefined context.
    Added test set get option with context.
    Added test basic format option.
    Added test consume format option.

--- option.test.js
+++ option.test.js
@@ -434,6 +434,371 @@ const TEST_OPTION_INSTANCE = (
 			}
 );
 
+const TEST_SET_GET_OPTION_ON_UNDEFINED_CONTEXT = (
+	async	function TEST_SET_GET_OPTION_ON_UNDEFINED_CONTEXT( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+
+				(
+					await	SETUP_TEST_DIRECTORY( )
+				);
+
+				try{
+					const option = (
+						Option( )
+						.option
+					);
+
+					(
+							option
+							.hello
+						=	(
+								"world"
+							)
+					);
+
+					const actualValue = (
+						option
+						.hello
+					);
+
+					const testValue = (
+						"world"
+					);
+
+					strictAssert
+					.equal(
+						(
+							actualValue
+						),
+
+						(
+							testValue
+						),
+
+						(
+							[
+								"#test-set-get-option-on-undefined-context;",
+
+								"test set get option on undefined context;",
+
+								`must be equal, ${ actualValue }, ${ testValue };`
+							]
+						)
+					);
+
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
+
+					return	(
+								false
+							);
+				}
+				finally{
+					(
+						await	CLEAN_TEST_DIRECTORY( )
+					);
+				}
+			}
+);
+
+const TEST_SET_GET_OPTION_WITH_CONTEXT = (
+	async	function TEST_SET_GET_OPTION_WITH_CONTEXT( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+
+				(
+					await	SETUP_TEST_DIRECTORY( )
+				);
+
+				try{
+					const option = (
+						Option(
+							(
+								{
+									"hello": (
+										"world"
+									)
+								}
+							)
+						)
+						.option
+					);
+
+					(
+							option
+							.hello
+						=	(
+								"mundo"
+							)
+					);
+
+					const actualValue = (
+						option
+						.hello
+					);
+
+					const testValue = (
+						"mundo"
+					);
+
+					strictAssert
+					.equal(
+						(
+							actualValue
+						),
+
+						(
+							testValue
+						),
+
+						(
+							[
+								"#test-set-get-option-with-context;",
+
+								"test set get option on undefined context;",
+
+								`must be equal, ${ actualValue }, ${ testValue };`
+							]
+						)
+					);
+
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
+
+					return	(
+								false
+							);
+				}
+				finally{
+					(
+						await	CLEAN_TEST_DIRECTORY( )
+					);
+				}
+			}
+);
+
+const TEST_BASIC_FORMAT_OPTION = (
+	async	function TEST_BASIC_FORMAT_OPTION( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+
+				(
+					await	SETUP_TEST_DIRECTORY( )
+				);
+
+				try{
+					const option = (
+						Option(
+							(
+								{
+									"hello": (
+										"world"
+									)
+								}
+							)
+						)
+						.option
+					);
+
+					option
+					.formatOption(
+						(
+							( { property, value } ) => (
+									(
+											(
+													property
+												===	"hello"
+											)
+									)
+								?	(
+										value
+										.toUpperCase( )
+									)
+								:	(
+										value
+									)
+							)
+						)
+					);
+
+					const actualValue = (
+						option
+						.hello
+					);
+
+					const testValue = (
+						"WORLD"
+					);
+
+					strictAssert
+					.equal(
+						(
+							actualValue
+						),
+
+						(
+							testValue
+						),
+
+						(
+							[
+								"#test-basic-format-option;",
+
+								"test basic format option;",
+
+								`must be equal, ${ actualValue }, ${ testValue };`
+							]
+						)
+					);
+
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
+
+					return	(
+								false
+							);
+				}
+				finally{
+					(
+						await	CLEAN_TEST_DIRECTORY( )
+					);
+				}
+			}
+);
+
+const TEST_CONSUME_FORMAT_OPTION = (
+	async	function TEST_CONSUME_FORMAT_OPTION( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+
+				(
+					await	SETUP_TEST_DIRECTORY( )
+				);
+
+				try{
+					const option = (
+						Option(
+							(
+								{
+									"hello": (
+										"world"
+									)
+								}
+							)
+						)
+						.option
+					);
+
+					(
+						option
+						.formatOption(
+							(
+								( { property, value } ) => (
+										(
+												(
+														property
+													===	"hello"
+												)
+										)
+									?	(
+											value
+											.toUpperCase( )
+										)
+									:	(
+											value
+										)
+								)
+							)
+						)
+						.hello
+					);
+
+					const actualValue = (
+						option
+						.hello
+					);
+
+					const testValue = (
+						"world"
+					);
+
+					strictAssert
+					.equal(
+						(
+							actualValue
+						),
+
+						(
+							testValue
+						),
+
+						(
+							[
+								"#test-consume-format-option;",
+
+								"test consume format option;",
+
+								`must be equal, ${ actualValue }, ${ testValue };`
+							]
+						)
+					);
+
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
+
+					return	(
+								false
+							);
+				}
+				finally{
+					(
+						await	CLEAN_TEST_DIRECTORY( )
+					);
+				}
+			}
+);
+
 (
 	async	function TEST_SCENE_BASIC( ){
 				(
@@ -446,11 +811,51 @@ const TEST_OPTION_INSTANCE = (
 						[
 							{
 								"test": (
-									"test option"
+									"test option instance"
 								),
 
 								"result": (
 									await	TEST_OPTION_INSTANCE( )
+								)
+							},
+
+							{
+								"test": (
+									"test set get option on undefined context"
+								),
+
+								"result": (
+									await	TEST_SET_GET_OPTION_ON_UNDEFINED_CONTEXT( )
+								)
+							},
+
+							{
+								"test": (
+									"test set get option with context"
+								),
+
+								"result": (
+										await	TEST_SET_GET_OPTION_WITH_CONTEXT( )
+								)
+							},
+
+							{
+								"test": (
+									"test basic format option"
+								),
+
+								"result": (
+										await	TEST_BASIC_FORMAT_OPTION( )
+								)
+							},
+
+							{
+								"test": (
+									"test consume format option"
+								),
+
+								"result": (
+										await	TEST_CONSUME_FORMAT_OPTION( )
 								)
 							}
 						]
